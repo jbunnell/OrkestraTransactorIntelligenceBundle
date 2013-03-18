72b36992485d4afc7545bcf29b47e3785fb9ac74
Peter Bunnell <jpeterbunnell@gmail.com>
2013-03-18 13:08:03 -0700

    base empty bundle installed

---
+++ OrkestraTransactorIntelligenceBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Orkestra\Bundle\TransactorIntelligenceBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class OrkestraTransactorIntelligenceBundle extends Bundle
+{
+}
